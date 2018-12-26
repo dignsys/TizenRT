9937406d43793aefb260a3288e90b9b29f3796ca
Han Yong Hee <yhhan61@gmail.com>
2018-12-26 17:53:50 +0900

    support flash ROMFS to make stm32_romfs_img.c

--- build/configs/stm32l4xr/mk_stm32_romfs.sh
+++ build/configs/stm32l4xr/mk_stm32_romfs.sh
@@ -1,5 +1,7 @@
-sh ../tools/fs/mkromfsimg.sh
-cd ../build/output/bin
+TOPDIR=$1
+rm ${TOPDIR}/build/output/bin/romfs.img
+sh ${TOPDIR}/tools/fs/mkromfsimg.sh
+cd ${TOPDIR}/build/output/bin
 xxd -i romfs.img > stm32_romfs_img.c
 cp stm32_romfs_img.c ../../../os/board/stm32l4xr/src
 echo "stm32_romfs_img.c was made"
